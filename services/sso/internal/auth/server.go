0f8efd382c6f95ddcbaa4674a2afacdf281d2f56
slavyakhin <slavyakhin@gmail.com>
2025-12-18 02:34:56 +0300

    add(sso): auth handlers service layer invocation

--- services/sso/internal/auth/server.go
+++ services/sso/internal/auth/server.go
@@ -10,12 +10,27 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-type serverAPI struct {
-	ssov1.UnimplementedAuthServer
+type Auth interface {
+	Login(
+		ctx context.Context,
+		email string,
+		password string,
+		appID int,
+	) (token string, err error)
+	RegisterNewUser(ctx context.Context,
+		email string,
+		password string,
+	) (userID int64, err error)
+	isAdmin(ctx context.Context, userID int64) (bool, error)
 }
 
-func Register(gRPC *grpc.Server) {
-	ssov1.RegisterAuthServer(gRPC, &serverAPI{})
+type serverAPI struct {
+	ssov1.UnimplementedAuthServer
+	auth Auth
+}
+
+func Register(gRPC *grpc.Server, auth Auth) {
+	ssov1.RegisterAuthServer(gRPC, &serverAPI{auth: auth})
 }
 
 const (
@@ -26,28 +41,38 @@ func (s *serverAPI) Register(
 	ctx context.Context,
 	req *ssov1.RegisterRequest,
 ) (*ssov1.RegisterResponse, error) {
-	panic("Implement me")
+	if err := validateRegister(req); err != nil {
+		return nil, err
+	}
+
+	userID, err := s.auth.RegisterNewUser(ctx, req.GetEmail(), req.GetPassword())
+	if err != nil {
+		// TODO: error handling
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &ssov1.RegisterResponse{
+		UserId: userID,
+	}, nil
 }
 
 func (s *serverAPI) Login(
 	ctx context.Context,
 	req *ssov1.LoginRequest,
 ) (*ssov1.LoginResponse, error) {
-	// validation
-	if _, err := mail.ParseAddress(req.GetEmail()); err != nil {
-		return nil, status.Error(codes.InvalidArgument, "invalid email")
-	}
-	if len(req.GetPassword()) < 8 {
-		return nil, status.Error(codes.InvalidArgument, "password length is less than 8")
-	}
-	if req.GetAppId() == emptyValue {
-		return nil, status.Error(codes.InvalidArgument, "app_id is required")
+
+	if err := validateLogin(req); err != nil {
+		return nil, err
 	}
 
-	// TODO: implement login via auth service
+	token, err := s.auth.Login(ctx, req.GetEmail(), req.GetPassword(), int(req.GetAppId()))
+	if err != nil {
+		// TODO: error handling
+		return nil, status.Error(codes.Internal, "internal error")
+	}
 
 	return &ssov1.LoginResponse{
-		Token: req.GetEmail(),
+		Token: token,
 	}, nil
 }
 
@@ -55,5 +80,52 @@ func (s *serverAPI) IsAdmin(
 	ctx context.Context,
 	req *ssov1.IsAdminRequest,
 ) (*ssov1.IsAdminResponse, error) {
-	panic("Implement me")
+
+	if err := validateIsAdmin(req); err != nil {
+		return nil, err
+	}
+
+	isAdmin, err := s.auth.isAdmin(ctx, req.GetUserId())
+	if err != nil {
+		// TODO: error handling
+		return nil, err
+	}
+
+	return &ssov1.IsAdminResponse{
+		IsAdmin: isAdmin,
+	}, nil
+}
+
+// Internal Methods
+func validateRegister(req *ssov1.RegisterRequest) error {
+	if _, err := mail.ParseAddress(req.GetEmail()); err != nil {
+		return status.Error(codes.InvalidArgument, "invalid email")
+	}
+	if len(req.GetPassword()) < 8 {
+		return status.Error(codes.InvalidArgument, "password length is less than 8")
+	}
+
+	return nil
+}
+
+func validateLogin(req *ssov1.LoginRequest) error {
+	if _, err := mail.ParseAddress(req.GetEmail()); err != nil {
+		return status.Error(codes.InvalidArgument, "invalid email")
+	}
+	if len(req.GetPassword()) < 8 {
+		return status.Error(codes.InvalidArgument, "password length is less than 8")
+	}
+	if req.GetAppId() == emptyValue {
+		return status.Error(codes.InvalidArgument, "app_id is required")
+	}
+
+	return nil
+}
+
+func validateIsAdmin(req *ssov1.IsAdminRequest) error {
+	if req.GetUserId() == emptyValue {
+		return status.Error(codes.InvalidArgument, "user_id is required")
+	}
+
+	return nil
 }
