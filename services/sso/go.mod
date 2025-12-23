170c052abc4e4451dfc59a1fae4005745eb052c6
slavyakhin <slavyakhin@gmail.com>
2025-12-24 02:42:38 +0300

    add(sso): data layer postgres storage struct

--- services/sso/go.mod
+++ services/sso/go.mod
@@ -2,19 +2,25 @@ module github.com/slavyakhin/EduTracker/services/sso
 
 go 1.24.4
 
-require github.com/slavyakhin/EduTracker/protos v0.0.1
+require (
+	github.com/golang-jwt/jwt/v5 v5.3.0
+	github.com/golang-migrate/migrate/v4 v4.19.1
+	github.com/ilyakaznacheev/cleanenv v1.5.0
+	github.com/slavyakhin/EduTracker/protos v0.0.1
+	golang.org/x/crypto v0.46.0
+	google.golang.org/grpc v1.77.0
+)
 
 require (
 	github.com/BurntSushi/toml v1.2.1 // indirect
-	github.com/golang-jwt/jwt/v5 v5.3.0 // indirect
-	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
+	github.com/jackc/pgx v3.6.2+incompatible // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
-	golang.org/x/crypto v0.46.0 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/net v0.47.0 // indirect
 	golang.org/x/sys v0.39.0 // indirect
 	golang.org/x/text v0.32.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20251022142026-3a174f9686a8 // indirect
-	google.golang.org/grpc v1.77.0 // indirect
 	google.golang.org/protobuf v1.36.11 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
---
+++ services/sso/internal/storage/postgres/postgres.go
@@ -0,0 +1,95 @@
+package postgres
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"github.com/jackc/pgx"
+	"github.com/slavyakhin/EduTracker/services/sso/internal/domain/models"
+	"github.com/slavyakhin/EduTracker/services/sso/internal/storage"
+)
+
+const (
+	UniqueViolation = "23505"
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+func New(storageURI string) (*Storage, error) {
+	const op = "storage.postgres.New"
+
+	db, err := sql.Open("pgx", storageURI)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &Storage{db: db}, nil
+}
+
+func (s *Storage) SaveUser(
+	ctx context.Context,
+	email string,
+	passHash []byte,
+) (int64, error) {
+	const op = "storage.postgres.SaveUser"
+
+	stmt, err := s.db.Prepare("INSERT INTO users(email, pass_hash) VALUES($1, $2)")
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	res, err := stmt.ExecContext(ctx, email, passHash)
+	if err != nil {
+		var pgxErr pgx.PgError
+
+		if errors.As(err, &pgxErr) && pgxErr.Code == UniqueViolation {
+			return 0, fmt.Errorf("%s: %w", op, storage.ErrUserExists)
+		}
+
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return id, nil
+}
+
+func (s *Storage) User(
+	ctx context.Context,
+	email string,
+) (models.User, error) {
+	const op = "storage.postgres.User"
+
+	stmt, err := s.db.Prepare("SELECT id, email, pass_hash FROM users WHERE email = $1")
+	if err != nil {
+		return models.User{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	row := stmt.QueryRowContext(ctx, email)
+
+	var user models.User
+	err = row.Scan(&user.ID, &user.Email, &user.PassHash)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return models.User{}, fmt.Errorf("%s: %w", op, storage.ErrUserNotFound)
+		}
+
+		return models.User{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return user, nil
+}
+
+func (s *Storage) IsAdmin(
+	ctx context.Context,
+	userID int64,
+) (bool, error) {
+
+}
