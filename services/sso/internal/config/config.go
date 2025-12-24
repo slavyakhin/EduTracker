87b0cdb14560cdfe874ab05f0da51b4eed1ad14f
slavyakhin <slavyakhin@gmail.com>
2025-12-24 03:10:28 +0300

    fix(sso): config StorageURI field

--- services/sso/internal/config/config.go
+++ services/sso/internal/config/config.go
@@ -10,7 +10,7 @@ import (
 
 type Config struct {
 	Env        string        `yaml:"env" env-default:"local"`
-	storageURI string        `yaml:"storage_uri" env-required:"true"`
+	StorageURI string        `yaml:"storage_uri" env-required:"true"`
 	TokenTTL   time.Duration `yaml:"token_ttl" env-required:"true"`
 	GRPC       GRPCConfig    `yaml:"grpc"`
 }
