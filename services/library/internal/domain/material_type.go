5f2707513346f148ad9cbaf81155fbe3222b6017
slavyakhin <slavyakhin@gmail.com>
2026-07-02 18:14:35 +0300

    refactor(library): domain material type change base type of MaterialType from string to int implement string method

--- services/library/internal/domain/material_type.go
+++ services/library/internal/domain/material_type.go
@@ -1,10 +1,30 @@
 package domain
 
-type MaterialType string
+import "fmt"
+
+type MaterialType int
 
 const (
-	MaterialImage    MaterialType = "image"
-	MaterialDocument MaterialType = "document"
-	MaterialText     MaterialType = "text"
-	MaterialVideo    MaterialType = "video"
+	MaterialUnknown MaterialType = iota
+	MaterialImage
+	MaterialDocument
+	MaterialText
+	MaterialVideo
 )
+
+func (mt MaterialType) String() string {
+	switch mt {
+	case MaterialUnknown:
+		return "UNKNOWN"
+	case MaterialImage:
+		return "IMAGE"
+	case MaterialDocument:
+		return "DOCUMENT"
+	case MaterialText:
+		return "TEXT"
+	case MaterialVideo:
+		return "VIDEO"
+	default:
+		return fmt.Sprintf("MaterialType(%d)", int(mt))
+	}
+}
